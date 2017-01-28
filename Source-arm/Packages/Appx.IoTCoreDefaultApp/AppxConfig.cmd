bad40d2a3d2be69cbd08d5ec1568c9cc3bafcb66
Paramesh Babu <pabab@microsoft.com>
2017-01-27 17:55:11 -0800

    IoTCoreDefaultApp install using provisioning package
    IoTCoreDefaultApp install using provisioning package

--- Source-arm/Packages/Appx.IoTCoreDefaultApp/AppxConfig.cmd
+++
@@ -1,5 +0,0 @@
-set AppxName=IoTCoreDefaultApp_1.1.0.0_ARM
-set certslist=IoTCoreDefaultApp_1.1.0.0_ARM  
-set dependencylist=Microsoft.NET.Native.Framework.1.3 Microsoft.NET.Native.Runtime.1.4 Microsoft.VCLibs.ARM.14.00  
-set forceinstall=0 
-set launchapp=1 
