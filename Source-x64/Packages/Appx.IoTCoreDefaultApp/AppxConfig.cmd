23cde3eb1b90d90080d569154634c17c83eed015
Paramesh Babu <pabab@microsoft.com>
2017-01-27 18:06:57 -0800

    IoTCoreDefaultApp Install with ppkg - x64
    IoTCoreDefaultApp Install with ppkg - x64

--- Source-x64/Packages/Appx.IoTCoreDefaultApp/AppxConfig.cmd
+++
@@ -1,5 +0,0 @@
-set AppxName=IoTCoreDefaultApp_1.1.0.0_x64
-set certslist=IoTCoreDefaultApp_1.1.0.0_x64  
-set dependencylist=Microsoft.NET.Native.Framework.1.3 Microsoft.NET.Native.Runtime.1.4 Microsoft.VCLibs.x64.14.00  
-set forceinstall=0 
-set launchapp=1 
