dd00c272d00c77a0f31c158327fca5b11a5aa843
Paramesh Babu <pabab@microsoft.com>
2017-06-29 17:51:38 -0700

    Adding non en-US single lang samples
    Adding non en-US single lang samples

---
+++ Source-x64/Products/SingleLangSample/oemcustomization.cmd
@@ -0,0 +1,12 @@
+@echo off
+REM OEM Customization Script file
+REM This script if included in the image, is called everytime the system boots.
+
+reg query HKLM\Software\IoT /v FirstBootDone >nul 2>&1
+
+if %errorlevel% == 1 (
+    REM Enable Administrator User
+    net user Administrateur p@ssw0rd /active:yes
+    reg add HKLM\Software\IoT /v FirstBootDone /t REG_DWORD /d 1 /f >nul 2>&1
+)
+
