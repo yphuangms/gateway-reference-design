ee2f17044ad13907ec4a55baf11e46fadea82888
Paramesh Babu <pabab@microsoft.com>
2017-07-17 13:51:29 -0700

    Provide option for manual provisioning, storing in user specified path
    Set PROV_PATH variable to configure the path where the ppkg needs to be
    stored. Eg.
    set PROV_PATH="$(runtime.root)\OEMInstall"

--- Tools/appx2pkg.cmd
+++ Tools/appx2pkg.cmd
@@ -54,9 +54,13 @@ if not defined PROV_VERSION if /i "%ADK_VERSION%" LSS "16190" (
 )
 
 if not defined PROV_RANK ( set PROV_RANK=0)
+if not defined PROV_PATH ( 
+    set PROV_PATH="$(runtime.windows)\Provisioning\Packages"
+)
 
 echo. Provisioning package version : %PROV_VERSION%
 echo.                      rank    : %PROV_RANK%
+echo.                      path    : %PROV_PATH%
 
 for %%A in (%STARTUP_OPTIONS%) do (
     if [%%A] == [%2] (
@@ -108,7 +112,7 @@ if defined LICENSE_FILE (
             set LICENSE_ID=%%j
         )
     )
-    echo.  LicenseProductID : !LICENSE_ID!
+    echo. LicenseProductID : !LICENSE_ID!
 )
 
 echo. Authoring %COMP_NAME%.%SUB_NAME%.pkg.xml
@@ -181,7 +185,7 @@ call :PRINT_TEXT "      <OSComponent>"
 call :PRINT_TEXT "         <Files>"
 REM Printing script files inclusion
 call :PRINT_TEXT "            <File Source="%COMP_NAME%.%SUB_NAME%.ppkg""
-echo                   DestinationDir="$(runtime.windows)\Provisioning\Packages">> "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml"
+echo                   DestinationDir=%PROV_PATH%>> "%FILE_PATH%\%COMP_NAME%.%SUB_NAME%.pkg.xml"
 call :PRINT_TEXT "                  Name="%COMP_NAME%.%SUB_NAME%.ppkg" />"
 call :PRINT_TEXT "         </Files>"
 call :PRINT_TEXT "      </OSComponent>"
