038d4d25a1609f71ffde25efe02ef0d09c94ef32
Paramesh Babu <pabab@microsoft.com>
2017-07-28 16:57:08 -0700

    Optimizing Recovery FFU creation
    Removed extractwim.cmd and collapsed its functionality in the
    buildrecovery.cmd itself.

--- Tools/buildrecovery.cmd
+++ Tools/buildrecovery.cmd
@@ -15,6 +15,13 @@ exit /b 1
 
 :START
 setlocal
+REM Input validation
+if [%1] == [/?] goto Usage
+if [%1] == [-?] goto Usage
+if [%1] == [] goto Usage
+if [%2] == [] goto Usage
+if /I not [%2] == [Retail] ( if /I not [%2] == [Test] goto Usage )
+
 if not defined PKGBLD_DIR (
     echo Environment not defined. Call setenv
     exit /b 1
@@ -22,21 +29,48 @@ if not defined PKGBLD_DIR (
 
 if not defined FFUNAME ( set FFUNAME=Flash)
 set OUTPUTDIR=%BLD_DIR%\%1\%2
-set IMG_FILE=%BLD_DIR%\%1\%2\%FFUNAME%.ffu
+set IMG_FILE=%OUTPUTDIR%\%FFUNAME%.ffu
+
 if not exist "%IMG_FILE%" (
     echo Building the base FFU
     call buildimage %1 %2
 )
-echo Extracting Wims
-call extractwim.cmd %1 %2
 
-copy %COMMON_DIR%\Packages\Recovery.Wimfiles\*.xml %OUTPUTDIR%\ >nul
-echo Building Recovery package
-call buildpkg %OUTPUTDIR%
+if not exist "%IMG_FILE%" (
+....REM File not found even after invoking buildimage. 
+    echo.%CLRRED%Error: Building the base FFU failed.%CLREND%
+)
+set IMG_RECOVERY_FILE=%OUTPUTDIR%\%FFUNAME%_Recovery.ffu
+echo Mounting %IMG_FILE% (this can take some time)..
+call wpimage mount "%IMG_FILE%" > %OUTPUTDIR%\mountlog.txt
 
-set FFUNAME=%FFUNAME%Recovery
-echo Building the RecoveryFFU
-call buildimage %1 %2
+REM This will break if there is space in the user account (eg.C:\users\test acct\)
+for /f "tokens=3,4,* skip=9 delims= " %%i in (%OUTPUTDIR%\mountlog.txt) do (
+    if [%%i] == [Path:] (
+        set MOUNT_PATH=%%j
+    ) else if [%%i] == [Name:] (
+        set DISK_DRIVE=%%j
+    )
+)
+
+echo Mounted at %MOUNT_PATH% as %DISK_DRIVE%..
+echo Extracting data wim
+dism /Capture-Image /ImageFile:%MOUNT_PATH%\mmos\data.wim /CaptureDir:%MOUNT_PATH%Data\ /Name:"DATA" /Compress:max
+echo Extracting MainOS wim, this can take a while too..
+dism /Capture-Image /ImageFile:%MOUNT_PATH%\mmos\mainos.wim /CaptureDir:%MOUNT_PATH% /Name:"MainOS" /Compress:max
+
+if exist %BSPSRC_DIR%\%BSP%\Packages\Recovery.WinPE\winpe.wim (
+    echo Copying winpe.wim..
+    copy %BSPSRC_DIR%\%BSP%\Packages\Recovery.WinPE\winpe.wim %MOUNT_PATH%\mmos
+) else (
+    echo.%CLRRED%Error:WinPE file not found. Recovery functionality will not work.%CLREND% 
+)
+
+echo %BSP_VERSION% > %MOUNT_PATH%\mmos\RecoveryImageVersion.txt
+
+echo Unmounting %DISK_DRIVE%
+wpimage dismount -physicaldrive %DISK_DRIVE% -imagepath %IMG_RECOVERY_FILE%
+REM del %OUTPUTDIR%\mountlog.txt
 
 endlocal
 exit /b
