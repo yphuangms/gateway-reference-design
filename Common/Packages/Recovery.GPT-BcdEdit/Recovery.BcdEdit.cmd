74b0b7d55cceb394b483d543844a3a52c26d09e7
Paramesh Babu <pabab@microsoft.com>
2017-08-04 14:44:22 -0700

    Recovery BCD files
    Recovery BCD files

---
+++ Common/Packages/Recovery.GPT-BcdEdit/Recovery.BcdEdit.cmd
@@ -0,0 +1,8 @@
+REM reg query HKLM\Software\IoT /v RecoveryBcdEdit >nul 2>&1
+REM if %errorlevel% == 1 (
+    bcdedit /set bootstatuspolicy IgnoreShutdownFailures
+    bcdedit /set recoveryenabled yes
+    bcdedit /set recoverysequence {a5935ff2-32ba-4617-bf36-5ac314b3f9bf}
+    reg add HKLM\Software\IoT /v RecoveryBcdEdit /t REG_DWORD /d 1 /f >nul 2>&1
+REM )
+
