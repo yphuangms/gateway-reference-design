ab88296261d5b7e8039af2f901949315979940e5
Paramesh Babu <pabab@microsoft.com>
2017-09-01 11:18:23 -0700

    Apply recovery settings on every boot
    Apply recovery settings on every boot , just in case an update over
    writes it.

--- Common/Packages/Recovery.GPT-BcdEdit/Recovery.BcdEdit.cmd
+++ Common/Packages/Recovery.GPT-BcdEdit/Recovery.BcdEdit.cmd
@@ -1,10 +1,11 @@
 @echo off
 
-reg query HKLM\Software\IoT /v RecoveryBcdEdit >nul 2>&1
+REM Settings are applied on every boot, in case it gets overrwritten during update
+REM reg query HKLM\Software\IoT /v RecoveryBcdEdit >nul 2>&1
 
-if %errorlevel% == 1 (
+REM if %errorlevel% == 1 (
     bcdedit /set bootstatuspolicy IgnoreShutdownFailures
     bcdedit /set recoveryenabled yes
     bcdedit /set recoverysequence {a5935ff2-32ba-4617-bf36-5ac314b3f9bf}
-    reg add HKLM\Software\IoT /v RecoveryBcdEdit /t REG_DWORD /d 1 /f >nul 2>&1
-)
+REM    reg add HKLM\Software\IoT /v RecoveryBcdEdit /t REG_DWORD /d 1 /f >nul 2>&1
+REM )
